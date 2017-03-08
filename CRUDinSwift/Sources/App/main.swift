e179ac8aa3ee3e9a86013b25427e9837ab83424d
Ralph Minor <ralphminor@Ralphs-MacBook-Pro.local>
2017-03-08 11:29:59 -0700

    Connected postgres and reated first simple GET route at http://localhost:8080/version.  Returns json with PG version info.

--- CRUDinSwift/Sources/App/main.swift
+++ CRUDinSwift/Sources/App/main.swift
@@ -1,11 +1,22 @@
 import Vapor
+import VaporPostgreSQL
 
 let drop = Droplet()
+try drop.addProvider(VaporPostgreSQL.Provider.self)
 
 drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
+}
+
+drop.get("version") { request in
+    if let db = drop.database?.driver as? PostgreSQLDriver {
+        let version = try db.raw("SELECT version()")
+        return try JSON(node: version)
+    } else {
+        return "No db connection"
+    }
 }
 
 drop.resource("posts", PostController())
