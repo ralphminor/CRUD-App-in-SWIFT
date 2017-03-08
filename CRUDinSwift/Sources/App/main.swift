fe2c7ab168f2cd3c33e64a517979137b0ebc3f12
Ralph Minor <ralphminor@Ralphs-MacBook-Pro.local>
2017-03-08 11:47:44 -0700

    Completed RESTful CRUD JSON API.  Can get/put/post/delete patient names.

--- CRUDinSwift/Sources/App/main.swift
+++ CRUDinSwift/Sources/App/main.swift
@@ -3,6 +3,7 @@ import VaporPostgreSQL
 
 let drop = Droplet()
 try drop.addProvider(VaporPostgreSQL.Provider.self)
+drop.preparations.append(Patient.self)
 
 drop.get { req in
     return try drop.view.make("welcome", [
@@ -17,6 +18,46 @@ drop.get("version") { request in
     } else {
         return "No db connection"
     }
+}
+
+drop.post("patients") { request in
+    var patient = try Patient(node: request.json)
+    try patient.save()
+    return patient
+}
+
+drop.get("patients", Patient.self) { request, patient in
+    return "You requested \(patient.firstName)"
+}
+
+drop.get("patients") { request in
+    return try JSON(node: Patient.all().makeNode())
+}
+
+drop.put("patients", Patient.self) { request, patient in
+    var patient = patient
+    guard let firstName = request.data["first_name"]?.string,
+        let lastName = request.data["last_name"]?.string else {
+            throw Abort.badRequest
+    }
+    
+    patient.firstName = firstName
+    patient.lastName = lastName
+    try patient.save()
+    return patient
+    
+}
+
+drop.get("patients", "fname", ":fname") { request in
+    guard let fname = request.parameters["fname"]?.string else {
+        throw Abort.badRequest
+    }
+    return try JSON(node: Patient.query().filter("first_name", fname).all().makeNode())
+}
+
+drop.delete("patients", Patient.self) { request, patient in
+    try patient.delete()
+    return try JSON(node: Patient.all().makeNode())
 }
 
 drop.resource("posts", PostController())
